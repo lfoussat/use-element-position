3f3d113f74c100e6bc89ab6b6dc7d682073bd905
Louise Foussat <louisefoussat@gmail.com>
2019-01-02 22:06:22 +0000

    First version of useElementPosition

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # use-element-position
+
 React hook to get bounding client rect
+
+## Usage
+
+```js
+import { useElementPosition } from 'use-element-position'
+
+const MyComponent = () => {
+  const [ref, { width }] = useElementPosition({ width: 0 })
+
+  return (
+    <div ref={ref}>
+      My width is {width}px
+    </div>
+  )
+}
+```
+
+Or use the wrapper component
+
+```js
+import { WithPosition } from 'use-element-position'
+
+const MyComponent = ({ width }) => (
+  <div>
+    My width is {width}px
+  </div>
+)
+
+const MyWrapperComponent = () => (
+  <WithPosition>
+    <MyComponent />
+  </WithPosition>
+)
+```
+
+You can retrieve all the properties included in [`getBoundingClientRect`](https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect) :
+bottom, height, left, right, top, width, x, y
+
+
---
+++ index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { useElementCallback } from 'use-element'
+
+const { ResizeObserver } = window
+
+const useElementPositionModern = callback =>
+  useElementCallback(elem => {
+    const observer = new ResizeObserver(() => {
+      callback(elem.getBoundingClientRect())
+    })
+
+    observer.observe(elem)
+
+    return () => observer.unobserve(elem)
+  })
+
+const useElementPositionLegacy = callback =>
+  useElementCallback(elem => {
+    // fallback to window resize
+    const onResize = () => {
+      callback(elem.getBoundingClientRect())
+    }
+
+    onResize() // initial value
+    window.addEventListener('resize', onResize, { passive: true }) // resized values
+
+    return () => window.removeEventListener('resize', onResize)
+  })
+
+export const useElementPositionCallback = ResizeObserver
+  ? useElementPositionModern
+  : useElementPositionLegacy
+
+export const useElementPosition = defaultValue => {
+  const [rect, setRect] = useState(defaultValue)
+  const ref = useElementPositionCallback(setRect)
+  return [ref, rect]
+}
+
+export const WithPosition = ({ children, ...props }) => {
+  const [ref, rect] = useElementPosition(new DOMRect())
+  const rectProps = {
+    bottom: rect.bottom,
+    height: rect.height,
+    left: rect.left,
+    right: rect.right,
+    top: rect.top,
+    width: rect.width,
+    x: rect.x,
+    y: rect.y
+  }
+
+  return React.createElement(
+    'div',
+    { ref, ...props },
+    React.Children.map(children, child => React.cloneElement(child, rectProps))
+  )
+}
