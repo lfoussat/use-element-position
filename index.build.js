893438ce4b224a1c60a73618b0fa5c59d8a9c87f
Clement Denis <le.mikmac@gmail.com>
2020-10-05 14:19:51 +0200

    rebuild index.build.js

--- index.build.js
+++ index.build.js
@@ -7,49 +7,13 @@ exports.WithPosition = exports.useElementPosition = exports.useElementPositionCa
 
 var _react = require("react");
 
-var _useElement = require("use-element");
-
 const {
   ResizeObserver
 } = window;
 
-const useElementPositionModern = callback => (0, _useElement.useElementCallback)(elem => {
-  const observer = new ResizeObserver(() => {
-    callback(elem.getBoundingClientRect());
-  });
-  observer.observe(elem);
-  return () => observer.unobserve(elem);
-});
-
-const useElementPositionLegacy = callback => (0, _useElement.useElementCallback)(elem => {
-  // fallback to window resize
-  const onResize = () => {
-    callback(elem.getBoundingClientRect());
-  };
-
-  onResize(); // initial value
-
-  window.addEventListener('resize', onResize, {
-    passive: true
-  }); // resized values
-
-  return () => window.removeEventListener('resize', onResize);
-});
-
-const useElementPositionCallback = exports.useElementPositionCallback = ResizeObserver ? useElementPositionModern : useElementPositionLegacy;
-
-const useElementPosition = exports.useElementPosition = defaultValue => {
-  const [rect, setRect] = (0, _react.useState)(defaultValue);
-  const ref = useElementPositionCallback(setRect);
-  return [ref, rect];
-};
-
-const WithPosition = exports.WithPosition = ({
-  children,
-  ...props
-}) => {
-  const [ref, rect] = useElementPosition(new DOMRect());
-  const rectProps = {
+const getElementPosition = elem => {
+  const rect = elem.getBoundingClientRect();
+  return {
     bottom: rect.bottom,
     height: rect.height,
     left: rect.left,
@@ -57,10 +21,133 @@ const WithPosition = exports.WithPosition = ({
     top: rect.top,
     width: rect.width,
     x: rect.x,
-    y: rect.y
+    y: rect.y,
+    offsetTop: elem.offsetTop,
+    offsetLeft: elem.offsetLeft
   };
+};
+
+const nextFrameSubs = new Set();
+
+const callAllFrameSubs = () => {
+  for (const sub of nextFrameSubs) {
+    sub();
+  }
+};
+
+let RAFId;
+
+const afterNextFrame = sub => {
+  if (!nextFrameSubs.size) {
+    nextFrameLoop();
+  }
+
+  nextFrameSubs.add(sub);
+  return () => {
+    nextFrameSubs.delete(sub);
+
+    if (!nextFrameSubs.size) {
+      cancelAnimationFrame(RAFId);
+    }
+  };
+};
+
+const nextFrameLoop = () => {
+  setTimeout(callAllFrameSubs, 0);
+  RAFId = requestAnimationFrame(nextFrameLoop);
+};
+
+const observeElem = (observers, elem, initialState) => {
+  const cachedObserver = observers.get(elem);
+  if (cachedObserver) return cachedObserver;
+  const newObserver = {
+    state: initialState,
+    callbacks: new Set()
+  };
+  observers.set(elem, newObserver);
+  return newObserver;
+};
+
+const triggerCallbacks = (callbacks, newState) => {
+  for (const cb of callbacks) {
+    cb(newState);
+  }
+};
+
+const observeDOM = (getter, hasChanged) => {
+  const observers = new Map();
+  let unsub;
+
+  const sub = () => {
+    for (const [elem, observer] of observers) {
+      const newState = getter(elem);
+
+      if (hasChanged(newState, observer.state)) {
+        observer.state = newState;
+        setTimeout(triggerCallbacks, 0, observer.callbacks, newState);
+      }
+    }
+  };
+
+  return (elem, callback, initialState) => {
+    if (!observers.size) {
+      unsub = afterNextFrame(sub);
+    }
+
+    const observe = observeElem(observers, elem, initialState);
+    observe.callbacks.add(callback);
+    return () => {
+      observe.callbacks.delete(callback);
+
+      if (!observe.callbacks.size) {
+        observers.delete(elem);
+
+        if (!observers.size) {
+          unsub();
+        }
+      }
+    };
+  };
+};
+
+const keys = ['width', 'height', 'top', 'right', 'bottom', 'left', 'offsetTop', 'offsetLeft'];
+const observeRect = observeDOM(elem => getElementPosition(elem), (a, b) => {
+  let i = -1;
+  if (a && !b) return true;
+
+  while (++i < keys.length) {
+    const key = keys[i];
+    if (a[key] !== b[key]) return true;
+  }
+
+  return false;
+});
+
+const useElementPositionCallback = exports.useElementPositionCallback = callback => elem => observeRect(elem, callback);
+
+const useElementPosition = exports.useElementPosition = defaultValue => {
+  const [rect, setRect] = (0, _react.useState)(defaultValue);
+  return [elem => observeRect(elem, setRect), rect];
+};
+
+const WithPosition = exports.WithPosition = ({
+  children,
+  ...props
+}) => {
+  const [ref, rect] = useElementPosition({
+    bottom: 0,
+    height: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    width: 0,
+    x: 0,
+    y: 0,
+    offsetTop: 0,
+    offsetLeft: 0
+  });
   return React.createElement('div', {
     ref,
     ...props
-  }, React.Children.map(children, child => React.cloneElement(child, rectProps)));
+  }, React.Children.map(children, child => React.cloneElement(child, rect)));
 };
